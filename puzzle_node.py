c4478d5762b1d94436ee7b800b0dc940e87e423a
MichalP <43441637+paulotheblack@users.noreply.github.com>
2020-03-23 14:13:43 +0100

    Delete puzzle_node.py

--- puzzle_node.py
+++
@@ -1,8 +0,0 @@
-import copy
-
-# Vrchol grafu
-class Node:
-    def __init__(self, puzzle, parent = None, last_operand = ""):
-        self.puzzle = copy.deepcopy(puzzle)
-        self.parent = parent
-        self.last_operand = last_operand
